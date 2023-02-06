7ddad4855b488d21af991390064c7657fd86efc3
Pavle Mojašević <pavle.mojasevic@2dsoft.com>
2023-02-06 08:54:14 +0100

    error controller

--- ServerApp/Controllers/ErrorController.cs
+++ ServerApp/Controllers/ErrorController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ServerApp.Models;
+using System.Net;
 
 namespace ServerApp.Controllers
 {
@@ -10,8 +13,19 @@ namespace ServerApp.Controllers
         [Route("/error")]
         public IActionResult HandleError()
         {
-            //TODO: exceptions
-            return BadRequest();
+            var exeptionHandlerFeature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            var exception = exeptionHandlerFeature.Error; 
+            ErrorResponse errorResponse = new ErrorResponse
+            {
+                Details = exception.StackTrace,
+                Title = exception.Message
+            };
+
+            var result = new ObjectResult(errorResponse)
+            {
+                StatusCode = (int)HttpStatusCode.InternalServerError,
+            };
+            return result;
         }
     }
 }
